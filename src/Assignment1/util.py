846eeba232206b43894bd15c2079cd5cafe8dc6f
Harish <harish.m@diggibyte.com>
2024-09-18 15:05:31 +0530

    finding the average percentage

---
+++ src/Assignment1/util.py
@@ -0,0 +1,20 @@
+
+def fnd_avg():
+    student={
+    "raj":[25,40,30],
+    "vijay":[50.45,20],
+    "krish":[35,30,32]
+    }
+
+    student_name=input("enter the student name:")
+    marks=student[student_name]
+   # total=sum(marks)
+    #avg=total/3
+    lst=0
+    for i in marks:
+        lst+=i
+    avg=lst/3
+    return avg
+
+l=fnd_avg()
+print(l)
