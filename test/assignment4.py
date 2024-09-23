69dcb296697751b756307e293857aa041a52532c
Harish <harish.m@diggibyte.com>
2024-09-23 11:02:27 +0530

    add the testcase file for 4th assignment

---
+++ test/assignment4.py
@@ -0,0 +1,12 @@
+import unittest
+
+from python_assignment.src.Assignment4.util import str_format
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(str_format(1),['1 1 1 1'] )  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
